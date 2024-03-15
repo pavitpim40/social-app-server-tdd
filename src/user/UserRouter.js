a630a930f70d08bd928a6fecb25e614d8e4096c5
pavitpim40_mbp <pavitpim@gmail.com>
2024-03-15 20:44:41 +0700

    feat:username validation

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,10 +1,26 @@
 const express = require('express');
 
 const UserService = require('./UserService');
-
 const router = express.Router();
 
 router.post('/api/1.0/users', async (req, res) => {
+  /*
+  {
+    validationErrors : {
+      username : 'Username cannot be null',
+      email : 'Email cannot be null',
+    }
+  }
+  
+  */
+  const user = req.body;
+  if (user.username === null) {
+    return res.status(400).send({
+      validationErrors: {
+        username: 'Username cannot be null',
+      },
+    });
+  }
   try {
     await UserService.save(req.body);
     return res.status(200).send({ message: 'User created' });
